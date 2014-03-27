94b6d3678fdd5d40310c36a122481eb5917e6db5
Chris Erin and Derek Parker <dev+chriserin+dparker1990@hashrocket.com>
2014-03-27 13:52:29 -0500

    Integration test for pear.

--- main_test.go
+++ main_test.go
@@ -10,6 +10,80 @@ func currentUser() string {
 	return user("--file", "fixtures/test.config")
 }
 
+func mockHomeEnv() {
+	os.Setenv("HOME", "fixtures/mocks")
+}
+
+func mockStdin(t *testing.T) {
+	fi, err := os.Open("fixtures/mocks/fullName.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os.Stdin = fi
+}
+
+func mockStdout(t *testing.T) (*os.File, *os.File) {
+	oldstdout := os.Stdout
+	tmp, err := ioutil.TempFile("", "")
+	if err != nil {
+		t.Error("Could not create temp file")
+	}
+
+	os.Stdout = tmp
+
+	return tmp, oldstdout
+}
+
+func cleanupStdout(t *testing.T, tmp *os.File, stdout *os.File) {
+	name := tmp.Name()
+	err := tmp.Close()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = os.Remove(name)
+	if err != nil {
+		t.Error(err)
+	}
+	os.Stdout = stdout
+}
+
+func restorePearrc(t *testing.T) {
+	fi, err := os.Open("fixtures/mocks/.pearrc")
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer fi.Close()
+
+	fi.Write([]byte("devs:\n\tdev1: Full Name A"))
+}
+
+func TestPear(t *testing.T) {
+	mockHomeEnv()
+	mockStdin(t)
+	tmp, oldstdout := mockStdout(t)
+	defer func() {
+		cleanupStdout(t, tmp, oldstdout)
+		restorePearrc(t)
+	}()
+
+	os.Args = []string{"pear", "dev1", "dev2"}
+
+	main()
+
+	conf, err := readPearrc("fixtures/mocks/.pearrc")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(conf.Devs) != 2 {
+		t.Error("Devs were not recorded")
+	}
+}
+
 func TestSetPairWithOneDev(t *testing.T) {
 	setPair([]string{"user1"}, "--file", "fixtures/test.config")
 	expected := "user1"
@@ -78,8 +152,7 @@ func TestSavePearrc(t *testing.T) {
 }
 
 func TestCheckPairWithUnknownDev(t *testing.T) {
-	oldStdout := os.Stdout
-	expectedFullName := "new developer"
+	expectedFullName := "Person B"
 	pair := []string{"knowndev", "newdev"}
 	conf := &Config{
 		Devs: map[string]string{
@@ -92,32 +165,10 @@ func TestCheckPairWithUnknownDev(t *testing.T) {
 		t.Error("Could not create temp file")
 	}
 
-	defer func() {
-		name := tmp.Name()
-		err := tmp.Close()
-
-		if err != nil {
-			t.Error(err)
-		}
-
-		err = os.Remove(name)
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-
-	os.Stdout = tmp
-
-	fi, err := os.Open("fixtures/fullName.txt")
-	if err != nil {
-		t.Error("Could not open file")
-	}
-
-	defer fi.Close()
-
-	os.Stdin = fi
+	mockStdin(t)
+	tmp, oldstdout := mockStdout(t)
+	defer cleanupStdout(t, tmp, oldstdout)
 	checkPair(pair, conf)
-	os.Stdout = oldStdout
 
 	_, err = tmp.Seek(0, os.SEEK_SET)
 	if err != nil {
