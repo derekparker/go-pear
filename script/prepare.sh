d0627dd024cd06d53a7d725f8347808be39923a0
Chris Erin and Derek Parker <dev+chriserin+dparker1990@hashrocket.com>
2014-04-21 15:35:08 -0500

    Update setup script
    Script will now run properly when libgit2/git2go has not been previously
    installed. Script will clone git2go and build libgit2 and set all
    environment variables accordingly.

--- script/prepare.sh
+++ script/prepare.sh
@@ -1,10 +1,18 @@
 set -x
 
-go get github.com/libgit2/git2go
+set PROJECT_DIR=$(pwd)
+git clone git://github.com/libgit2/git2go.git $GOPATH/src/github.com/git2go
 
 chmod +x $GOPATH/src/github.com/libgit2/git2go/script/build-libgit2.sh
-$GOPATH/src/github.com/libgit2/git2go/script/build-libgit2.sh
 
-export LD_LIBRARY_PATH=libgit2/install/lib
-export DYLD_LIBRARY_PATH=libgit2/install/lib
-export PKG_CONFIG_PATH=libgit2/install/lib/pkgconfig
+cd $GOPATH/src/github.com/libgit2/git2go
+script/build-libgit2.sh
+
+set LIBGIT2_LIB=$GOPATH/src/libgit2/git2go/libgit2/install/lib
+export LD_LIBRARY_PATH=$LIBGIT2_LIB
+export DYLD_LIBRARY_PATH=$LIBGIT2_LIB
+export PKG_CONFIG_PATH=$LIBGIT2_LIB/pkgconfig
+
+go install github.com/libgit2/git2go
+
+cd PROJECT_DIR
