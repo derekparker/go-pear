200cbfba46b1038c3ee03ff4fd856bb1618185fc
Chris Erin <chris.erin+chriserin@gmail.com>
2018-02-25 18:27:38 -0600

    Create build script
    Co-authored-by: Chris Erin <chris.erin+chriserin@gmail.com>

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p builds
+version=`git describe --abbrev=0 --tags`
+echo $version
+env GOOS=linux GOARCH=386 go build -o builds/pear-linux-$version
+echo "built linux version"
+env GOOS=darwin GOARCH=386 go build -o builds/pear-macos-$version
+echo "built macos version"
+env GOOS=windows GOARCH=386 go build -o builds/pear-windows-$version.exe
+echo "built windows version"
